1e0f26ee799a0c54c7f0220fb580270e1e4897b4
Joe <nastillmatic@gmail.com>
2022-07-26 19:08:34 +0900

    initial commit

---
+++ Cafe/CafeApp.swift
@@ -0,0 +1,17 @@
+//
+//  CafeApp.swift
+//  Cafe
+//
+//  Created by HwangByungJo  on 2022/07/26.
+//
+
+import SwiftUI
+
+@main
+struct CafeApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
